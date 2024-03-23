f83ff47c4f38c5850f88dc4134d7e21e8cb3990a
ZhongLiDai <d1226394303@gmail.com>
2024-03-23 05:27:11 -0700

    Create product_list.php

---
+++ product_list.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <title>Document</title>
+</head>
+<body>
+    
+<header class="navbar">
+    <div class="container display flex"><div class="flex-one">
+        <h1>Store</h1>
+    </div></div>
+    <div class="flex-stretch"></div>
+<nav class="nav-flex flex-one">
+    <ul>
+        
+    </ul>
+</nav>
+</body>
+</html>
