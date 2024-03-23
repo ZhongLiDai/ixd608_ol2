9d70146fa817f5ea4b062365ebd3aaa453b53f29
ZhongLiDai <d1226394303@gmail.com>
2024-03-23 08:05:02 -0700

    Create index.php

---
+++ game shop/index.php
@@ -0,0 +1,78 @@
+<?php 
+$title = "Homepage"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title?></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div id="nav">
+        <ul class="nav">
+            <li><a href="">Game</a></li>
+            <li><a href="">New</a></li>
+            <li><a href="">Discount</a></li>
+            <li><a href="">Type</a></li>
+            <li><a href="">Service</a></li>
+        </ul>
+    </div>
+    <div class="banner"></div>
+    <h1 class="best">Best selling games</h3>
+        <div class="gallery">
+            <ul>
+                <li class="game1">
+                    <div class="game-thumbnail"></div>
+                    <p class="game-title">Granblue Fantasy: Relink</p>
+                </li>
+                <li class="game2">
+                    <div class="game-thumbnail"></div>
+                    <p class="game-title">Sekiro: Shdow Die Twice</p>
+                </li>
+                <li class="game3">
+                    <div class="game-thumbnail"></div>
+                    <p class="game-title">The Binding of Issac: Rebirth</p>
+                </li>
+                <li class="game4">
+                    <div class="game-thumbnail"></div>
+                    <p class="game-title">The Lies of P</p>
+                </li>
+                <li class="game5">
+                    <div class="game-thumbnail"></div>
+                    <p class="game-title">Monster Hunter World</p>
+                </li>
+            </ul>
+        </div>
+        <div class="signup">
+            <h2>Sign up to choose your games</h2>
+            <input type="email"><button>sign up</button>
+        </div>
+        <h1>Hot deal</h1>
+        <div class="gameshow">
+            <ul>
+                <li class="box1"></li>
+                <li class="box2"></li>
+                <li class="box3"></li>
+                <li class="box4"></li>
+                <li class="box5"></li>
+                <li class="box6"></li>
+                <li class="box7"></li>
+                <li class="box8"></li>
+            </ul>
+        </div>
+        <div class="footer">
+            <p>Support</p>
+            <p>Privacy Policy</p>
+            <p>Website Terms of Use</p>
+            <p>legal</p>
+            <p>About</p>
+            <p>@2024 GameShop LLC</p>
+        </div>
+</body>
+
+</html>
